aa4a78e1e147d1d7abc323fab39000f3327c9c95
Dmitry Arkhipov <grisumbras@gmail.com>
2019-08-20 22:36:28 +0300

    fixed doctype

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -8,7 +8,7 @@ eval "$(pyenv virtualenv-init -)"
 
 mkdir -p build
 conan config set general.request_timeout=600
-echo '!doctype html' >build/index.html
+echo '<!doctype html>' >build/index.html
 echo '<html><body><pre>' >>build/index.html
 conan search '*' -r conan-center >>build/index.html
 echo '</pre></body></html>' >>build/index.html
