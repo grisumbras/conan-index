9ca0ed39313055250ce15f17c071f5064d5bdeee
Dmitry Arkhipov <grisumbras@gmail.com>
2019-08-21 01:01:41 +0300

    investigating

--- .github/actions/get-packages/entrypoint.sh
+++ .github/actions/get-packages/entrypoint.sh
@@ -9,3 +9,4 @@ eval "$(pyenv virtualenv-init -)"
 conan config set general.request_timeout=600
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 # conan search '*' -r all -j packages.json
+ls $HOME -la
