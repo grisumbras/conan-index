15dc0cbe3801b7ed3060ed40a6622bfcb30d425d
Dmitry Arkhipov <grisumbras@gmail.com>
2019-08-21 12:57:33 +0300

    new layout

--- index.mako
+++ index.mako
@@ -4,13 +4,46 @@
   </head>
   <body>
     <h1>Conan Package Index</h1>
-    % for remote in data:
-      <h2>${remote.name} (${remote.url})</h2>
-      <ul>
-        % for package in remote.packages:
-          <li>${package["recipe"]["id"]}</li>
-        % endfor
-      </ul>
-    % endfor
+    <ul>
+      % for group in data["package_groups"]:
+        <li>
+          <h3>${group.name}@${group.namespace}</h3>
+          <p>${group.description}</p>
+          % if group.topics:
+            Topics:
+            <ul>
+              % for topic in group.topics:
+                <li>${topic}</li>
+              % endfor
+            </ul>
+          % endif
+          <dl>
+            % if group.homepage:
+              <dt>Project hompepage</dt><dd>${group.homepage}</dd>
+            % endif
+            % if group.url:
+              <dt>Package homepage</dt><dd>${group.url}</dd>
+            % endif
+            <dt>License</dt><dd>${group.license or "proprietary"}</dd>
+            <dt>Author</dt><dd>${group.author or "-"}</dd>
+          </dl>
+          Packages:
+          <ol>
+            % for package in group.packages:
+              <li>
+                ${package.reference()}
+                (on <a href="#remote-${package.remote.name}">${package.remote.name}</a>)
+              </li>
+            % endfor
+          </ol>
+        </li>
+      % endfor
+    </ul>
+    <h2>Remotes</h2>
+    <ul>
+      % for remote in data["remotes"]:
+        <li id="remote-${remote.name}">${remote.name} ${remote.url}</li>
+      % endfor
+    </ul>
   </body>
 </html>
--- render.py
+++ render.py
@@ -4,31 +4,147 @@
 from mako.template import Template
 from mako.runtime import Context
 from pathlib import Path
-import sys
 import json
+import re
+import subprocess
+import sys
 
 
 class Remote(object):
-    def __init__(self, name, url, packages):
+    def __init__(self, name, url):
         self.name = name
         self.url = url
-        self.packages = packages
+        self.packages = list()
+
+    def __lt__(self, other):
+        return self.name < other.name
+
+    def __le__(self, other):
+        return self.name <= other.name
+
+    def __eq__(self, other):
+        return self.name == other.name
 
 
-def get_data(remotes, packages):
-    packages = json.load(packages)["results"]
-    remotes = json.load(remotes)["remotes"]
+class Package(object):
+    def __init__(self, name, version, namespace, channel, remote):
+        self.name = name
+        self.version = version
+        self.namespace = namespace
+        self.channel = channel
+        self.remote = remote
+
+    def reference(self):
+        return "%s/%s@%s/%s" % (
+            self.name, self.version, self.namespace, self.channel
+        )
+
+    def _struct(self):
+        return (self.name, self.namespace, self.version, self.channel)
+
+    def __lt__(self, other):
+        return self._struct() < other._struct()
+
+    def __le__(self, other):
+        return self._struct() <= other._struct()
+
+    def __eq__(self, other):
+        return self._struct() == other._struct()
+
+
+class PackageGroup(object):
+    def __init__(self, name, namespace):
+        self.name = name
+        self.namespace = namespace
+        self.packages = list()
+
+    def load_info(self):
+        package = self.packages[0]
+        # subprocess.call((
+        #     "conan",
+        #     "inspect",
+        #     "-r", package.remote.name,
+        #     "-j", "/tmp/package.json",
+        #     package.reference(),
+        # ))
+        with open("/tmp/package.json", "r") as package_file:
+            data = json.load(package_file)
+            self.url = data["url"]
+            self.homepage = data["homepage"]
+            self.license = data["license"]
+            self.author = data["author"]
+            self.description = data["description"]
+            self.topics = data["topics"]
+
+
+    def _struct(self):
+        return (self.name, self.namespace)
+
+    def __lt__(self, other):
+        return self._struct() < other._struct()
+
+    def __le__(self, other):
+        return self._struct() <= other._struct()
+
+    def __eq__(self, other):
+        return self._struct() == other._struct()
+
+
+def get_remotes(remotes_file):
+    remotes_data = json.load(remotes_file)["remotes"]
+    return dict((
+        (remote["name"], Remote(name=remote["name"], url=remote["url"]))
+        for remote in remotes_data
+    ))
+
+
+def get_packages(packages_file, remotes):
+    packages_data = json.load(packages_file)["results"]
+
+    pattern = re.compile("^([-\w.]+)/([-\w.]+)@([-\w.]+)/([-\w.]+)$")
     result = list()
-    for remote in packages:
-        remote_name = remote["remote"]
-        remote_url = [r["url"] for r in remotes if r["name"] == remote_name][0]
-        result.append(Remote(
-            name=remote_name,
-            url=remote_url,
-            packages=remote["items"]
-        ))
-    result = sorted(result, key=lambda r: r.name)
+    for remote_data in packages_data:
+        remote = remotes[remote_data["remote"]]
+        for package_data in remote_data["items"]:
+            match = pattern.match(package_data["recipe"]["id"])
+            if not match:
+                continue
+            result.append(Package(
+                *match.groups(),
+                remote=remote,
+            ))
     return result
+
+
+def group_packages(packages):
+    groups = dict()
+    for package in packages:
+        group_id = (package.name, package.namespace)
+        group = groups.setdefault(group_id, PackageGroup(*group_id))
+        group.packages.append(package)
+
+    groups = sorted(groups.values())
+    for group in groups:
+        group.packages = sorted(group.packages, reverse=True)
+        group.load_info()
+    return groups
+
+
+def get_data(remotes_file, packages_file):
+    remotes = get_remotes(remotes_file)
+    packages = get_packages(packages_file, remotes)
+
+    for package in packages:
+        remotes[package.remote.name].packages.append(package)
+
+    for remote in remotes.values():
+        remote.packages = sorted(remote.packages)
+
+    remotes = [
+        remote for remote in sorted(remotes.values()) if remote.packages
+    ]
+    package_groups = group_packages(packages)
+    return dict(remotes=remotes, package_groups=package_groups)
 
 
 def main():
